fb8fea6b5fa3e255aaa6d18630a28fd4ac35bcf1
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-05-02 20:33:05 +0000

    fix

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Basic Auth module
+"""
+
+from api.v1.auth.auth import Auth
+from typing import TypeVar, List
+from models.user import User
+import base64
+import binascii
+
+
+class BasicAuth(Auth):
+    """
+    class BasicAuth
+    """
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """returns the Base64"""
+        if (authorization_header is None or
+                not isinstance(authorization_header, str) or
+                not authorization_header.startswith("Basic")):
+            return None
+
+        return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns the decoded value"""
+        b64_auth_header = base64_authorization_header
+        if b64_auth_header and isinstance(b64_auth_header, str):
+            try:
+                encode = b64_auth_header.encode('utf-8')
+                base = base64.b64decode(encode)
+                return base.decode('utf-8')
+            except binascii.Error:
+                return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """returns the user"""
+        decoded_64 = decoded_base64_authorization_header
+        if (decoded_64 and isinstance(decoded_64, str) and
+                ":" in decoded_64):
+            res = decoded_64.split(":", 1)
+            return (res[0], res[1])
+        return (None, None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Display overloads Auth"""
+        header = self.authorization_header(request)
+        b64header = self.extract_base64_authorization_header(header)
+        decoded = self.decode_base64_authorization_header(b64header)
+        user_creds = self.extract_user_credentials(decoded)
+        return self.user_object_from_credentials(*user_creds)
