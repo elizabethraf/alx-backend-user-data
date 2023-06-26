3cf985b14471214224e6bb3266443e9a3d8ccd3c
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-06-26 09:40:58 +0000

    user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-"""Display SQLAlchemy model named User for a database"""
+"""Display User Model"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
 
 class User(Base):
-    """Display a Base Class"""
+    """Describe a user """
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
