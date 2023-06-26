0f796de26ed24b23c954d0cce1241f6b5efd5142
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-06-26 10:08:21 +0000

    correctionappo

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,18 +1,135 @@
 #!/usr/bin/env python3
-"""Display  Log out"""
-from flask import request, redirect, jsonify
-from werkzeug.exceptions import Forbidden
+"""Display API Routes for Authentication Service"""
+from auth import Auth
+from flask import (Flask,
+                   jsonify,
+                   request,
+                   abort,
+                   redirect)
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def hello_world() -> str:
+    """Base route for authentication service API """
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """Registers a new user"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
+
+
+@app.route('/sessions', methods=['POST'])
+def log_in() -> str:
+    """Logs in a user"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    msg = {"email": email, "message": "logged in"}
+    response = jsonify(msg)
+
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 @app.route('/sessions', methods=['DELETE'])
-def logout():
-    session_id = request.cookies.get('session_id')
+def log_out() -> str:
+    """Find the user"""
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """Display User profile"""
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    msg = {"email": user.email}
+
+    return jsonify(msg), 200
+
+
+@app.route('/reset_password', methods=['POST'])
+def reset_password() -> str:
+    """Reset password"""
+    try:
+        email = request.form['email']
+    except KeyError:
+        abort(403)
 
     try:
-        user = User.query.filter_by(session_id=session_id).one()
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
 
-        user.session_id = None
-        db.session.commit()
-        return redirect('/')
-    except:
-        raise Forbidden(description='Invalid session ID.')
+    msg = {"email": email, "reset_token": reset_token}
+
+    return jsonify(msg), 200
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """Updates password with reset token"""
+    try:
+        email = request.form['email']
+        reset_token = request.form['reset_token']
+        new_password = request.form['new_password']
+    except KeyError:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    msg = {"email": email, "message": "Password updated"}
+    return jsonify(msg), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
