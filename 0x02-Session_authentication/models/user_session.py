0d9d6b994b47653dad62d6b1f6c0082517f27b07
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-06-26 12:17:54 +0000

    user_fix

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -7,7 +7,7 @@ class UserSession(Base):
     """User Class"""
 
     def __init__(self, *args: list, **kwargs: dict):
-        ""Display self"""
+        """Display self"""
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
